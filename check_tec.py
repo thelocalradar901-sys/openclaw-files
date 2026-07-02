c049741a75caa60d89af4af845d32bee6d7dc046
thelocalradar901-sys <thelocalradar901@gmail.com>
2026-07-01 22:05:18 -0500

    Add files via upload

---
+++ check_tec.py
@@ -0,0 +1,10 @@
+import json
+p = json.load(open('/tmp/tec_filtered.json'))
+print('total_events:', p.get('total_events'))
+print('total_pages:', p.get('total_pages'))
+print('count:', len(p.get('events', [])))
+if p.get('events'):
+    print('first title:', p['events'][0]['title'])
+    print('first date:', p['events'][0].get('start_date'))
+    print('last title:', p['events'][-1]['title'])
+    print('last date:', p['events'][-1].get('start_date'))
